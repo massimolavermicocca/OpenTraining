c18f8e3ff0748640cb746205f59684419dcccb0e
Francesco <francesco.colaianni96@gmail.com>
2017-07-02 19:18:57 +0200

    Bug javadoc

--- app/src/de/skubware/opentraining/activity/acra/OpenTrainingApplication.java
+++ app/src/de/skubware/opentraining/activity/acra/OpenTrainingApplication.java
@@ -3,23 +3,10 @@ package de.skubware.opentraining.activity.acra;
 import android.app.Application;
 
 import org.acra.ACRA;
-import org.acra.ReportingInteractionMode;
-import org.acra.annotation.ReportsCrashes;
-
 import de.skubware.opentraining.BuildConfig;
-import de.skubware.opentraining.R;
-
-@ReportsCrashes(
-        formKey = "", // This is required for backward compatibility but not used
-        mode = ReportingInteractionMode.DIALOG,
-        resDialogText = R.string.crash_dialog_text,
-        resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. when defined, adds a user text field input with this text resource as a label
-        resDialogOkToast = R.string.crash_dialog_ok_toast // optional. displays a Toast message when the user accepts to send a report.
-
-)
 
 /**
- * Class OpenTrainingApplication
+ * @class OpenTrainingApplication
  */
 public class OpenTrainingApplication extends Application{
 
